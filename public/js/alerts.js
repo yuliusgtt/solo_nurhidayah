042c42992379b15f350a737b2af697a8fc95fa5a
gatotpur <yulius.gtt@gmail.com>
2025-01-16 14:33:37 +0700

    update alert icon

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,7 +1,13 @@
 function errorAlert(Message) {
+    const fillColor = '#ff4d49';
+    const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${fillColor}"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM12 10.5858L14.8284 7.75736L16.2426 9.17157L13.4142 12L16.2426 14.8284L14.8284 16.2426L12 13.4142L9.17157 16.2426L7.75736 14.8284L10.5858 12L7.75736 9.17157L9.17157 7.75736L12 10.5858Z"></path></svg>`;
+
     Swal.fire({
         html: Message,
-        icon: "error",
+        // icon: "error",
+        imageUrl: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgIcon),
+        imageWidth: 100,
+        imageHeight: 100,
         buttonsStyling: false,
         confirmButtonText: "Ok",
         // focusConfirm: true,
@@ -18,14 +24,20 @@ function errorAlert(Message) {
 }
 
 function successAlert(Message) {
+    const fillColor = '#72e128';
+    const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${fillColor}"><path d="M4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM17.4571 9.45711L16.0429 8.04289L11 13.0858L8.20711 10.2929L6.79289 11.7071L11 15.9142L17.4571 9.45711Z"></path></svg>`;
+
     Swal.fire({
         html: Message,
-        icon: "success",
+        // icon: "success",
+        imageUrl: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgIcon),
+        imageWidth: 100,
+        imageHeight: 100,
         buttonsStyling: false,
         confirmButtonText: "Ok",
         // focusConfirm: true,
         customClass: {
-            confirmButton: "btn btn-primary"
+            confirmButton: "btn btn-success"
         },
         didOpen: (popup) => {
             const okButton = popup.querySelector('.swal2-confirm');
@@ -37,9 +49,15 @@ function successAlert(Message) {
 }
 
 function warningAlert(Message) {
+    const fillColor = '#fdb528';
+    const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${fillColor}"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path></svg>`;
+
     Swal.fire({
         html: Message,
-        icon: "warning",
+        // icon: "warning",
+        imageUrl: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(svgIcon),
+        imageWidth: 100,
+        imageHeight: 100,
         buttonsStyling: false,
         confirmButtonText: "Ok",
         // focusConfirm: true,
