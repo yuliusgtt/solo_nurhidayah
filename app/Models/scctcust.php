bde6755b96067598956a3fc2527d7f4de5276100
gatotpur <yulius.gtt@gmail.com>
2025-03-10 16:17:27 +0700

    update columns

--- app/Models/scctcust.php
+++ app/Models/scctcust.php
@@ -30,9 +30,11 @@ class scctcust extends Model
         'DESC05',
         'TOTPAY',
         'GENUS',
+        'GENUS1',
         'LastUpdate',
         'GetWisma',
         'GENUSContact',
+        'EksternalInternal'
     ];
 
     public static function showVA($nis): string
